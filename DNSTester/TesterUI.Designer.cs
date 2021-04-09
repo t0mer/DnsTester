63203ddb076e8dabfbef03297cc6a6b522244e66
T0mer <tomer.klein@gmail.com>
2021-04-09 17:43:42 +0300

    First Commit

---
+++ DNSTester/TesterUI.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace DNSTester
+{
+    partial class TesterUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TesterUI));
+            this.ResultView = new System.Windows.Forms.ListView();
+            this.WebName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DNS1IP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DNS1Timing = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DNS2IP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DNS2Timing = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.RunTest = new MetroFramework.Controls.MetroButton();
+            this.lblDns1 = new MetroFramework.Controls.MetroLabel();
+            this.lblDns2 = new MetroFramework.Controls.MetroLabel();
+            this.DnsList1 = new MetroFramework.Controls.MetroComboBox();
+            this.DnsList2 = new MetroFramework.Controls.MetroComboBox();
+            this.UseCustomServers = new MetroFramework.Controls.MetroCheckBox();
+            this.lblCustom1 = new MetroFramework.Controls.MetroLabel();
+            this.lblCustom2 = new MetroFramework.Controls.MetroLabel();
+            this.CustomDns1 = new MetroFramework.Controls.MetroTextBox();
+            this.CustomDns2 = new MetroFramework.Controls.MetroTextBox();
+            this.lslStatus = new MetroFramework.Controls.MetroLabel();
+            this.StatusBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ResultView
+            // 
+            this.ResultView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.WebName,
+            this.DNS1IP,
+            this.DNS1Timing,
+            this.DNS2IP,
+            this.DNS2Timing});
+            this.ResultView.GridLines = true;
+            this.ResultView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.ResultView.HideSelection = false;
+            this.ResultView.Location = new System.Drawing.Point(58, 159);
+            this.ResultView.MultiSelect = false;
+            this.ResultView.Name = "ResultView";
+            this.ResultView.Size = new System.Drawing.Size(831, 318);
+            this.ResultView.TabIndex = 11;
+            this.ResultView.UseCompatibleStateImageBehavior = false;
+            this.ResultView.View = System.Windows.Forms.View.Details;
+            // 
+            // WebName
+            // 
+            this.WebName.Text = "URL";
+            this.WebName.Width = 160;
+            // 
+            // DNS1IP
+            // 
+            this.DNS1IP.Text = "IP Address from DNS 1";
+            this.DNS1IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DNS1IP.Width = 140;
+            // 
+            // DNS1Timing
+            // 
+            this.DNS1Timing.Text = "DNS 1 Timing";
+            this.DNS1Timing.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DNS1Timing.Width = 120;
+            // 
+            // DNS2IP
+            // 
+            this.DNS2IP.Text = "IP Address from DNS 2";
+            this.DNS2IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DNS2IP.Width = 140;
+            // 
+            // DNS2Timing
+            // 
+            this.DNS2Timing.Text = "DNS 2 Timing";
+            this.DNS2Timing.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DNS2Timing.Width = 120;
+            // 
+            // RunTest
+            // 
+            this.RunTest.Location = new System.Drawing.Point(424, 483);
+            this.RunTest.Name = "RunTest";
+            this.RunTest.Size = new System.Drawing.Size(75, 23);
+            this.RunTest.TabIndex = 12;
+            this.RunTest.Text = "Run Test";
+            this.RunTest.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.RunTest.Click += new System.EventHandler(this.RunTest_Click);
+            // 
+            // lblDns1
+            // 
+            this.lblDns1.AutoSize = true;
+            this.lblDns1.Location = new System.Drawing.Point(58, 70);
+            this.lblDns1.Name = "lblDns1";
+            this.lblDns1.Size = new System.Drawing.Size(42, 19);
+            this.lblDns1.TabIndex = 13;
+            this.lblDns1.Text = "Dns 1:";
+            // 
+            // lblDns2
+            // 
+            this.lblDns2.AutoSize = true;
+            this.lblDns2.Location = new System.Drawing.Point(411, 70);
+            this.lblDns2.Name = "lblDns2";
+            this.lblDns2.Size = new System.Drawing.Size(44, 19);
+            this.lblDns2.TabIndex = 14;
+            this.lblDns2.Text = "Dns 2:";
+            // 
+            // DnsList1
+            // 
+            this.DnsList1.FormattingEnabled = true;
+            this.DnsList1.ItemHeight = 23;
+            this.DnsList1.Location = new System.Drawing.Point(104, 65);
+            this.DnsList1.Name = "DnsList1";
+            this.DnsList1.Size = new System.Drawing.Size(286, 29);
+            this.DnsList1.TabIndex = 15;
+            // 
+            // DnsList2
+            // 
+            this.DnsList2.FormattingEnabled = true;
+            this.DnsList2.ItemHeight = 23;
+            this.DnsList2.Location = new System.Drawing.Point(461, 65);
+            this.DnsList2.Name = "DnsList2";
+            this.DnsList2.Size = new System.Drawing.Size(298, 29);
+            this.DnsList2.TabIndex = 16;
+            // 
+            // UseCustomServers
+            // 
+            this.UseCustomServers.AutoSize = true;
+            this.UseCustomServers.Location = new System.Drawing.Point(784, 70);
+            this.UseCustomServers.Name = "UseCustomServers";
+            this.UseCustomServers.Size = new System.Drawing.Size(105, 15);
+            this.UseCustomServers.TabIndex = 17;
+            this.UseCustomServers.Text = "Custom Servers";
+            this.UseCustomServers.UseVisualStyleBackColor = true;
+            this.UseCustomServers.CheckedChanged += new System.EventHandler(this.UseCustomServers_CheckedChanged);
+            // 
+            // lblCustom1
+            // 
+            this.lblCustom1.AutoSize = true;
+            this.lblCustom1.Location = new System.Drawing.Point(58, 123);
+            this.lblCustom1.Name = "lblCustom1";
+            this.lblCustom1.Size = new System.Drawing.Size(95, 19);
+            this.lblCustom1.TabIndex = 18;
+            this.lblCustom1.Text = "Custom Dns 1: ";
+            this.lblCustom1.Visible = false;
+            // 
+            // lblCustom2
+            // 
+            this.lblCustom2.AutoSize = true;
+            this.lblCustom2.Location = new System.Drawing.Point(411, 123);
+            this.lblCustom2.Name = "lblCustom2";
+            this.lblCustom2.Size = new System.Drawing.Size(97, 19);
+            this.lblCustom2.TabIndex = 19;
+            this.lblCustom2.Text = "Custom Dns 2: ";
+            this.lblCustom2.Visible = false;
+            // 
+            // CustomDns1
+            // 
+            this.CustomDns1.BackColor = System.Drawing.Color.White;
+            this.CustomDns1.Enabled = false;
+            this.CustomDns1.Location = new System.Drawing.Point(153, 121);
+            this.CustomDns1.Name = "CustomDns1";
+            this.CustomDns1.Size = new System.Drawing.Size(237, 23);
+            this.CustomDns1.TabIndex = 20;
+            this.CustomDns1.Visible = false;
+            // 
+            // CustomDns2
+            // 
+            this.CustomDns2.Enabled = false;
+            this.CustomDns2.Location = new System.Drawing.Point(506, 121);
+            this.CustomDns2.Name = "CustomDns2";
+            this.CustomDns2.Size = new System.Drawing.Size(253, 23);
+            this.CustomDns2.TabIndex = 21;
+            this.CustomDns2.Visible = false;
+            // 
+            // lslStatus
+            // 
+            this.lslStatus.AutoSize = true;
+            this.lslStatus.Location = new System.Drawing.Point(58, 511);
+            this.lslStatus.Name = "lslStatus";
+            this.lslStatus.Size = new System.Drawing.Size(46, 19);
+            this.lslStatus.TabIndex = 22;
+            this.lslStatus.Text = "Status:";
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.FormattingEnabled = true;
+            this.StatusBox.HorizontalScrollbar = true;
+            this.StatusBox.Location = new System.Drawing.Point(58, 533);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(831, 160);
+            this.StatusBox.TabIndex = 23;
+            // 
+            // TesterUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(944, 710);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.lslStatus);
+            this.Controls.Add(this.CustomDns2);
+            this.Controls.Add(this.CustomDns1);
+            this.Controls.Add(this.lblCustom2);
+            this.Controls.Add(this.lblCustom1);
+            this.Controls.Add(this.UseCustomServers);
+            this.Controls.Add(this.DnsList2);
+            this.Controls.Add(this.DnsList1);
+            this.Controls.Add(this.lblDns2);
+            this.Controls.Add(this.lblDns1);
+            this.Controls.Add(this.RunTest);
+            this.Controls.Add(this.ResultView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TesterUI";
+            this.Text = "DNSTester";
+            this.Load += new System.EventHandler(this.TesterUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ResultView;
+        private System.Windows.Forms.ColumnHeader WebName;
+        private System.Windows.Forms.ColumnHeader DNS1IP;
+        private System.Windows.Forms.ColumnHeader DNS1Timing;
+        private System.Windows.Forms.ColumnHeader DNS2IP;
+        private System.Windows.Forms.ColumnHeader DNS2Timing;
+        private MetroFramework.Controls.MetroButton RunTest;
+        private MetroFramework.Controls.MetroLabel lblDns1;
+        private MetroFramework.Controls.MetroLabel lblDns2;
+        private MetroFramework.Controls.MetroComboBox DnsList1;
+        private MetroFramework.Controls.MetroComboBox DnsList2;
+        private MetroFramework.Controls.MetroCheckBox UseCustomServers;
+        private MetroFramework.Controls.MetroLabel lblCustom1;
+        private MetroFramework.Controls.MetroLabel lblCustom2;
+        private MetroFramework.Controls.MetroTextBox CustomDns1;
+        private MetroFramework.Controls.MetroTextBox CustomDns2;
+        private MetroFramework.Controls.MetroLabel lslStatus;
+        private System.Windows.Forms.ListBox StatusBox;
+    }
+}
+
---
+++ DNSTester/TesterUI.cs
@@ -0,0 +1,380 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DNSTester
+{
+    public partial class TesterUI : MetroForm
+    {
+        private bool UseCustomDnsServers = false;
+        public string DNS1, DNS2;
+
+        public TesterUI()
+        {
+            InitializeComponent();
+        }
+
+        private void RunTest_Click(object sender, EventArgs e)
+        {
+            int i;
+            string[] CleanNames = new string[500];
+            int CleanNamesCount = 0;
+
+            if (this.UseCustomDnsServers)
+            {
+                DNS1 = CustomDns1.Text;
+                DNS2 = CustomDns2.Text;
+            }
+            else
+            {
+                DNS1 = DnsList1.SelectedValue.ToString();
+                DNS2 = DnsList2.SelectedValue.ToString();
+
+            }
+
+			ResultView.Items.Clear();
+			ResultView.Update();
+			Application.DoEvents();
+			
+			GetWebAddresses(ref CleanNames, ref CleanNamesCount);
+
+			for (i = 0; i < CleanNamesCount; i++)
+			{
+				ListViewItem listURL = new ListViewItem(CleanNames[i]);
+				listURL.SubItems.Add(" ");
+				listURL.SubItems.Add(" ");
+				listURL.SubItems.Add(" ");
+				listURL.SubItems.Add(" ");
+
+				ResultView.Items.Add(listURL);
+			}
+			ResultView.EnsureVisible(ResultView.Items.Count - 1);
+
+			ResultView.Update();
+			Application.DoEvents();
+
+			CheckDNS(CleanNames, CleanNamesCount, DNS1, DNS2);
+
+		}
+
+
+		private void TesterUI_Load(object sender, EventArgs e)
+        {
+            FillDnsList();
+        }
+
+        private void FillDnsList()
+        {
+            DnsList1.ValueMember = "ip";
+            DnsList1.DisplayMember = "name";
+
+            DnsList2.ValueMember = "ip";
+            DnsList2.DisplayMember = "name";
+
+            DataSet servers1 = new DataSet();
+            DataSet servers2 = new DataSet();
+            servers1.ReadXml(Path.Combine(Application.StartupPath, "Servers.xml"));
+            servers2.ReadXml(Path.Combine(Application.StartupPath, "Servers.xml"));
+            DataTable ns1 = servers1.Tables[0];
+            DataTable ns2 = servers2.Tables[0];
+            DnsList1.DataSource = ns1;
+            DnsList2.DataSource = ns2;
+
+        }
+
+
+        private void UseCustomServers_CheckedChanged(object sender, EventArgs e)
+        {
+            this.UseCustomDnsServers = UseCustomServers.Checked;
+            SetUI();
+        }
+
+        private void SetUI()
+        {
+            if (this.UseCustomDnsServers)
+            {
+                this.DnsList1.Enabled = false;
+                this.DnsList2.Enabled = false;
+
+                this.CustomDns1.Enabled = true;
+                this.CustomDns2.Enabled = true;
+
+                this.CustomDns1.Visible = true;
+                this.CustomDns2.Visible = true;
+                this.lblCustom1.Visible = true;
+                this.lblCustom2.Visible = true;
+
+            }
+            else
+            {
+                this.DnsList1.Enabled = true;
+                this.DnsList2.Enabled = true;
+                this.CustomDns1.Enabled = false; ;
+                this.CustomDns2.Enabled = false;
+
+                this.CustomDns1.Visible = false;
+                this.CustomDns2.Visible = false;
+                this.lblCustom1.Visible = false;
+                this.lblCustom2.Visible = false;
+
+            }
+        }
+
+
+
+		private void GetWebAddresses(ref string[] CleanNames, ref int CleanNamesCount)
+		{
+			int i, j;
+			byte[] random = new Byte[256];
+			string Letters = "";
+			// Make a 3 letter random string
+
+			//RNGCryptoServiceProvider is an implementation of a random number generator.
+			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+			rng.GetBytes(random); // The array is now filled with cryptographically strong random bytes.
+			for (i = 1; i < 256; i++)
+			{
+				// Just take the first three letters
+				if (random[i] > 0x40 && random[i] < 0x5b) Letters = Letters + Convert.ToChar(random[i]);
+				if (Letters.Length == 3) break;
+			}
+
+			// Use Google to get random addresses for DNS testing
+
+			// Initialize the WebRequest.
+		
+			StatusBoxPrint("Getting Google request");
+			try
+			{
+				WebRequest GoogleRequest = WebRequest.Create("http://www.google.com/search?num=100&q=" + Letters); //Letters);
+				WebResponse GoogleResponse = GoogleRequest.GetResponse();
+				Application.DoEvents();
+				// Get the response stream.
+				Stream GoogleStream = GoogleResponse.GetResponseStream();
+				StatusBoxPrint("Google response received");
+				// Use a StreamReader to read the entire response.
+
+				StreamReader GoogleReader = new StreamReader(GoogleStream, Encoding.ASCII);
+				string GoogleString = GoogleReader.ReadToEnd();
+				// Close the response stream and response to free resources.
+				GoogleStream.Close();
+				GoogleResponse.Close();
+				Application.DoEvents();
+
+				// find all .xxx.com addresses
+				MatchCollection HostNames;
+				string[] Names = new string[1000];
+				// Create a new Regex object and define the regular expression.
+				Regex Dotcom = new Regex("[.]([A-Za-z]*)[.]com");
+				// Use the Matches method to find all matches in the input string.
+				HostNames = Dotcom.Matches(GoogleString);
+
+				Application.DoEvents();
+				// Loop through the match collection to retrieve all 
+				// matches and delete the leading "."
+				for (i = 0; i < HostNames.Count; i++)
+				{
+					Names[i] = HostNames[i].Value.Remove(0, 1);
+				}
+				// eliminate google.com and repeated entries
+				for (i = 0; i < HostNames.Count - 1; i++)
+				{
+					if (Names[i] == "google.com") Names[i] = "";
+					for (j = i + 1; j < HostNames.Count; j++)
+					{
+						if (Names[i] == Names[j]) Names[j] = "";
+					}
+				}
+				// count list and compact
+				j = 0;
+				for (i = 0; i < HostNames.Count; i++)
+				{
+					if (Names[i] != "")
+					{
+						CleanNames[j] = Names[i];
+						j++;
+					}
+				}
+				CleanNamesCount = j;
+				StatusBoxPrint(CleanNamesCount + " Random URL's found");
+
+			}
+			catch
+			{
+				StatusBoxPrint("Host Google.com not found - are you connected ?");
+			}
+
+		}
+
+		private void CheckDNS(string[] URLNames, int URLNamescount, string DNSAddress1, string DNSAddress2)
+		{
+			int i;
+			const int IPPort = 53;
+			const string TransactionID1 = "Q1"; // Use transaction ID of Q1 and Q2 to identify our packet and DNS
+			const string TransactionID2 = "Q2";
+			const string TypeString = "\u0001" + "\u0000" + "\u0000" + "\u0001" + "\u0000" + "\u0000" + "\u0000" + "\u0000" + "\u0000" + "\u0000";
+			const string TrailerString = "\u0000" + "\u0000" + "\u0001" + "\u0000" + "\u0001";
+			const int DNSReceiveTimeout = 5000;
+			string URLNameStart, DomainName, QueryString, ReceiveString, IPResponse, sDeltaTime;
+			int URLNameStartLength, DomainNameLength, index, TransactionDNS;
+			byte[] Sendbytes = new byte[256];
+			long StartTime, StopTime;
+			string DeltaTime;
+			Socket DNSsocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			DNSsocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, DNSReceiveTimeout);
+			IPEndPoint dnsEP1 = new IPEndPoint(IPAddress.Parse(DNSAddress1), IPPort);
+			IPEndPoint dnsEP2 = new IPEndPoint(IPAddress.Parse(DNSAddress2), IPPort);
+
+			// Start the clock
+			StartTime = DateTime.Now.Ticks;
+
+			// i=0; // for testing - do one
+			for (i = 0; i < URLNamescount; i++)
+			{
+				URLNameStart = URLNames[i].Substring(0, URLNames[i].IndexOf("."));
+				DomainName = URLNames[i].Substring(URLNames[i].IndexOf(".") + 1, URLNames[i].Length - URLNames[i].IndexOf(".") - 1);
+				// Build the query 
+				QueryString = TransactionID1 + TypeString + (char)URLNameStart.Length + URLNameStart + (char)DomainName.Length + DomainName + TrailerString;
+				Sendbytes = Encoding.ASCII.GetBytes(QueryString);
+				DNSsocket.SendTo(Sendbytes, Sendbytes.Length, SocketFlags.None, dnsEP1);
+
+				// send the same message to both DNS servers except for the transaction ID
+				QueryString = TransactionID2 + TypeString + (char)URLNameStart.Length + URLNameStart + (char)DomainName.Length + DomainName + TrailerString;
+				Sendbytes = Encoding.ASCII.GetBytes(QueryString);
+				DNSsocket.SendTo(Sendbytes, Sendbytes.Length, SocketFlags.None, dnsEP2);
+			}
+
+			byte[] Receivebytes = new byte[512];
+
+			try
+			{
+			// wait for a response up to timeout
+			more: DNSsocket.Receive(Receivebytes);
+
+
+				// make sure the message returned is ours
+				if (Receivebytes[0] == Sendbytes[0] && (Receivebytes[1] == 0x31) || (Receivebytes[1] == 0x32))
+				{
+					if (Receivebytes[2] == 0x81 && Receivebytes[3] == 0x80)
+					{
+						// Get the time now
+						StopTime = DateTime.Now.Ticks;
+						DeltaTime = Convert.ToString((double)(StopTime - StartTime) / 10000000);
+
+						// Decode the answers
+						// Find the URL that was returned
+						TransactionDNS = Receivebytes[1];
+						ReceiveString = Encoding.ASCII.GetString(Receivebytes);
+						index = 12;
+						URLNameStartLength = Receivebytes[index];
+						index++;
+						URLNameStart = ReceiveString.Substring(index, URLNameStartLength);
+						index = index + URLNameStartLength;
+						DomainNameLength = Receivebytes[index];
+						index++;
+						DomainName = ReceiveString.Substring(index, DomainNameLength);
+						index = index + DomainNameLength;
+						index = index + 8;
+
+						// Get the record type
+						int ResponseType = Receivebytes[index];
+						index = index + 9;
+
+						// Get the IP address if applicable
+						IPResponse = "";
+						switch (ResponseType)
+						{
+							case 1:
+								IPResponse = Convert.ToString(Receivebytes[index]) + "."
+									+ Convert.ToString(Receivebytes[index + 1]) + "."
+									+ Convert.ToString(Receivebytes[index + 2]) + "."
+									+ Convert.ToString(Receivebytes[index + 3]); break;
+							case 5: IPResponse = "CNAME"; break;
+							case 6: IPResponse = "SOA"; break;
+						}
+						StatusBoxPrint("DNS Answer: " + URLNameStart + "." + DomainName + " = " + IPResponse);
+
+						// Find the URL entry in the list
+						for (i = 0; i < ResultView.Items.Count; i++)
+						{
+							if (ResultView.Items[i].Text == URLNameStart + "." + DomainName)
+							{
+
+								switch (TransactionDNS)
+								{
+									case 0x31:
+										ResultView.Items[i].SubItems[1].Text = Convert.ToString(IPResponse);
+										sDeltaTime = Convert.ToString(DeltaTime);
+										if (sDeltaTime.Length > 5) ResultView.Items[i].SubItems[2].Text = sDeltaTime.Substring(0, 5);
+										else ResultView.Items[i].SubItems[2].Text = sDeltaTime;
+										ResultView.Items[i].ForeColor = System.Drawing.Color.Red;
+										ResultView.EnsureVisible(i);
+										ResultView.Update();
+										Application.DoEvents();
+										ResultView.Items[i].ForeColor = System.Drawing.Color.Black;
+										break;
+
+									case 0x32:
+										ResultView.Items[i].SubItems[3].Text = Convert.ToString(IPResponse);
+										sDeltaTime = Convert.ToString(DeltaTime);
+										if (sDeltaTime.Length > 5) ResultView.Items[i].SubItems[4].Text = sDeltaTime.Substring(0, 5);
+										else ResultView.Items[i].SubItems[4].Text = sDeltaTime;
+										ResultView.Items[i].ForeColor = System.Drawing.Color.Red;
+										ResultView.EnsureVisible(i);
+										ResultView.Update();
+										Application.DoEvents();
+										ResultView.Items[i].ForeColor = System.Drawing.Color.Black;
+										break;
+								}
+							}
+						}
+					}
+					goto more;
+				}
+			}
+			catch (SocketException e)
+			{
+				if (Convert.ToString(e).IndexOf("time") > 0)
+				{
+					StatusBoxPrint("Timeout - No response received for " + Convert.ToString(DNSReceiveTimeout / 1000) + " seconds");
+				}
+				else
+				{
+					StatusBoxPrint(Convert.ToString(e)); // for testing	
+				}
+			}
+			finally
+			{
+				// close the socket
+				DNSsocket.Close();
+			}
+
+		}
+
+		private void StatusBoxPrint(string LogText)
+		{
+			StatusBox.Items.Add(DateTime.Now + "  " + LogText);
+			StatusBox.TopIndex = StatusBox.Items.Count - 1;
+			if (StatusBox.Items.Count > 5000)
+			{
+				StatusBox.Items.RemoveAt(0);
+			}
+			StatusBox.Update();
+		}
+
+
+	}
+}
